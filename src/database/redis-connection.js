6d5607442d8cf8f26c3882d066e43e72c08ffae1
cdsanchezm <cristiansanchezmalagon19@gmail.com>
2022-04-17 22:49:05 -0500

    reddis part 2

--- src/database/redis-connection.js
+++ src/database/redis-connection.js
@@ -1,9 +1,12 @@
 const redis = require("ioredis");
 const colors = require("colors");
 const { set } = require("mongoose");
+const { request, response } = require('express');
 const axios = require('axios');
 const db = require('../database/postgres-connection');
 const {promisify} = require('util');
+const {addProduct, deleteProduct,getCarrito} = require('../controllers/carrito.controller');
+const res = require("express/lib/response");
 
 
 const redisClient = new redis({
@@ -99,10 +102,147 @@ const redisConnection = async() => {
          
     }
 
+    //agregar producto al carrito y a redis
+    const addToCarrito = async(req = request, res = response) => {
+        try {
+            const {variante_id,cantidad} = req.body;
+            const {carrito_id} = req.params;
+            const ttl = 3600;
+            const reply = await get_async(`carrito_${carrito_id}`);
+            if(reply){
+                const carrito = JSON.parse(reply);
+                const producto = await db.query(`SELECT * FROM variante WHERE variante_id = ${variante_id}`);
+                const producto_id = producto.rows[0].producto_id;
+                const producto_nombre = producto.rows[0].titulo;
+                const producto_imagen = producto.rows[0].imagen;
+                const Cantidad = cantidad;
+                const producto_precio = producto.rows[0].precio; 
+                const producto_variante_id = variante_id;
+                const producto_variante_nombre = producto.rows[0].nombre;
+
+                const producto_variante = {
+                    producto_id,
+                    producto_nombre,
+                    producto_imagen,
+                    producto_precio,
+                    Cantidad,
+                    producto_variante_id,
+                    producto_variante_nombre
+                }
+                
+                carrito.push(producto_variante);
+                await set_async(`carrito_${carrito_id}`, JSON.stringify(carrito), 'EX', ttl);
+                return res.status(200).json({
+                    ok: true,
+                    message: 'Producto agregado al carrito',
+                    carrito: carrito
+                });
+            }else{
+                const producto = await db.query(`SELECT * FROM variante WHERE variante_id = ${variante_id}`);
+                const producto_id = producto.rows[0].producto_id;
+                const producto_nombre = producto.rows[0].titulo;
+                const producto_imagen = producto.rows[0].imagen;
+                const producto_precio = producto.rows[0].precio;
+                const Cantidad = cantidad;
+                const producto_variante_id = variante_id;
+                const producto_variante_nombre = producto.rows[0].nombre;
+                
+                const producto_variante = {
+                    producto_id,
+                    producto_nombre,
+                    producto_imagen,
+                    producto_precio,
+                    Cantidad,
+                    producto_variante_id,
+                    producto_variante_nombre
+                }
+                const carrito = [];
+                carrito.push(producto_variante);
+                await set_async(`carrito_${carrito_id}`, JSON.stringify(carrito), 'EX', ttl);
+                return res.status(200).json({
+                    ok: true,
+                    message: 'Producto agregado al carrito con exito ',   
+                    carrito: carrito
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({
+                ok: false,
+                message: 'Error en el servidor',
+                error
+            });
+        }
+    }
+
+    //obtener carrito de redis con informacion personalizada
+    const getCarritoo = async(req = request, res = response) => {
+        try {
+            const {carrito_id} = req.params;
+            const ttl = 3600;
+            const reply = await get_async(`carrito_${carrito_id}`);
+            if(reply){
+                const carrito = JSON.parse(reply);
+                let products=[];
+                for(let index in carrito){
+
+                    const product = carrito[index];
+                    const producto = await db.query(`SELECT * FROM variante WHERE variante_id = ${carrito[index].variante_id}`);
+                    const marca = await db.query(`SELECT * FROM marca WHERE marca_id = ${producto.rows[0].marca_id}`);
+                    const proveedor = await db.query(`SELECT * FROM proveedor WHERE proveedor_id = ${producto.rows[0].proveedor_id}`);
+                    const usuario = await db.query(`SELECT * FROM usuario WHERE usuario_id = ${proveedor.rows[0].usuario_id}`);
+
+                    
+                    const producto_variante = {
+                        producto_id : producto.rows[0].producto_id,
+                        producto_nombre: producto.rows[0].titulo,
+                        producto_imagen: producto.rows[0].imagen,
+                        producto_preciov: producto.rows[0].precio,
+                        producto_cantidad: product.Cantidad,
+                        proveedor_nombre: usuario.rows[0].nombres,
+                        marca_nombre: marca.rows[0].nombre
+                    }
+                  
+                    products.push(producto_variante);
+                   
+                }
+                return res.status(200).json({
+                    ok: true,
+                    message: 'Carrito obtenido con exito',
+                    carrito: products
+                });
+            }else{
+                return res.status(200).json({
+                    ok: true,
+                    message: 'Carrito vacio',
+                    carrito: []
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({
+                ok: false,
+                message: 'Error en el servidor',
+                error
+            });
+        }
+    }
+    
+                
+
+
+                    
+
+
+
+         
+
+
   
 
     module.exports = {
         redisConnection,
-        getAll
+        getAll,
+        addToCarrito,
+        getCarritoo
     }
-
--- src/routes/redis.routes.js
+++ src/routes/redis.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 
-const {redisConnection,getAll} = require('../database/redis-connection');
+const {redisConnection,getAll,addToCarrito,getCarritoo} = require('../database/redis-connection');
 
 router.get('/getAll', getAll);
+router.post('/addToCarrito/:carrito_id', addToCarrito);
+router.get('/getCarritoo/:carrito_id', getCarritoo);
 
 
 module.exports = router;
